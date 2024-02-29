1dfb1b1581ae5465b796189179e18889b9ab1fa5
hutechnang <160829545+hutechnang@users.noreply.github.com>
2024-02-29 16:20:02 +0700

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,8 @@
+package DST;
+puplic class Car {
+    private String  color;
+    private int price;
+    private String brand;
+    puplic int getPrice(){ return price; }
+    puplic String getBrand() {return brand; }
+}
