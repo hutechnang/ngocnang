1f7bd571f527ee85a214b700779325d11aa2ebe8
hutechnang <160829545+hutechnang@users.noreply.github.com>
2024-02-29 16:19:28 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package BST;
+public class Main {
+    puplic static void main(String[] args) {
+        Car car = new Car();
+        ToyataCar toyataCar = new ToyataCar();
+        System.out.println("stop!");
+    }
+}
---
+++ ToaDo.java
@@ -0,0 +1,13 @@
+public class ToaDo {
+    private int x;
+    private int y;
+    protected int z;
+    int m;//default
+    puplic int n;
+
+    puplic ToaDo(int x,int y){
+        if(x > 0)
+            this.x = x;
+        else this.x = 0;
+    }
+}
